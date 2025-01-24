8f3d7c7bb310aaebf424cbc6f33ac368f05fbcda
AfonsoJ3 <afonsoj3@wit.edu>
2025-01-24 14:41:35 -0500

    second commit

--- PA1/mm-student.c
+++ PA1/mm-student.c
@@ -3,11 +3,21 @@
 #include "mm-student.h"
 #include <time.h>
 
+void printMatrix(int n, double* matrix);
+
+
+
+
 //Main function
 int main(int argc, char **argv)
 {
-	int n = 10;
+	int n;
+	printf("Enter a size of the matrix\n>>");
+	scanf("%d",&n);
 
+	//Generate a number that is in a rnage
+	srand(time(NULL));
+	
 	//first array and second array
 	double* f_matrix = (double*)malloc(n*n*sizeof(double));
 	double* s_matrix = (double*)malloc(n*n*sizeof(double));
@@ -23,50 +33,62 @@ int main(int argc, char **argv)
 	assignMatrix(s_matrix, n);
 	
 	double* mMatrix = (double* )malloc(n*n*sizeof(double));
-	mMtrix = mm(f_matrix, m_matrix, n);
+	mMatrix = mm(f_matrix, s_matrix, n);
 	
-
-	for (int i = 0; i < n; i++)
-	{
-		printf("Array%d: %f\n", i, f_matrix[i]);
-	}
+	printMatrix(n, f_matrix);
+	printMatrix(n, s_matrix);
+	printMatrix(n, mMatrix);
 
 	free(f_matrix);
 	free(s_matrix);
-	free(mMatrix)
+	free(mMatrix);
 	return 0;
 }
+
 
 void assignMatrix(double* mat, int n)
 {
 	int min = -100;
 	int max = 100;
 
-	//Generate a number that is in a rnage
-	srand(time(NULL));
-
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
-			 mat [i * n + j]= rand() % (max - min + 1) + min;
+			mat [i * n + j]= rand() % (max - min + 1) + min;
+			//mat[i * n + j] = j;
 		}
 	}
+	
 }
 
 double* mm(double* A, double* B, int n)
 {
-	double* mult = (double* ) malloc(n*n* sizeof(double))
+	double* mult = (double* ) malloc(n*n* sizeof(double));
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 		{
+			mult[i * n + j] = 0;
 			for (int k = 0; k < n; k++)
 			{
-				mult[i*n+j] =A[i*n+k] * B[k*n +j]; 
+				mult[i*n+j] +=A[i*n+k] * B[k*n +j]; 
 			}
 		}
 	}
 	return mult;
 }
+
+void printMatrix(int n, double* matrix) {
+    for (int i = 0; i < n; i++) 
+	{
+        for (int j = 0; j < n; j++) 
+		{
+            printf("col%d, row%d: %6.2f\n",i+1,j+1,matrix[i * n + j]); // Properly format as a matrix
+        }
+       
+    }
+    printf("\n");
+}
+
 
