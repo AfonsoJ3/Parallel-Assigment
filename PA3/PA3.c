13dff664974e61dff3aba76450d79942529adcc3
AfonsoJ3 <afonsoj3@wit.edu>
2025-02-10 14:10:49 -0500

    PA3 test

--- PA3/PA3.c
+++ PA3/PA3.c
@@ -2,13 +2,44 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+//Change it to a bigger mumber. 
+#define SIZE 3
 int main(int argc, char** argv)
 {
 
     	int rank,numranks ;
+
+		int* m; //matrix pointer
+		int* v; //vector pointer
+
     	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     	MPI_Comm_size(MPI_COMM_WORLD,&numranks);
 
+		if(rank == 0)
+		{
+			m = (int*)malloc(SIZE*SIZE*sizeof(int));
+			v = (int*)malloc(1*SIZE*sizeof(int));
+
+			for (int i = 0; i < SIZE*SIZE; i++)
+			{
+				if (i < Size - 1)
+				{
+					v[i] = i + 1;
+				}
+
+				m [i] = i + 1; 
+			}
+		}
+
+		for (int i = 0; i < SIZE*SIZE; i++)
+		{
+			printf("%d\n", m[i]);
+		}
+
+		for (int i = 0; i < SIZE; i++)
+		{
+			printf("%d\n", v[i]);
+		}
 
     	MPI_Finalize();
 
