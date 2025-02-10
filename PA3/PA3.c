6e9b20ea84beb78c69be2ca413da306e10e2547d
AfonsoJ3 <afonsoj3@wit.edu>
2025-02-10 17:13:53 -0500

    PA3 Fixes V2

--- PA3/PA3.c
+++ PA3/PA3.c
@@ -4,66 +4,69 @@
 
 //Change it to a bigger mumber. 
 #define SIZE 3
+
 int main(int argc, char** argv)
 {
 
-    	int rank,numranks ;
+	int rank,numranks ;
 
 	int* m; //matrix pointer
 	int* v; //vector pointer
 	int* result;
 	MPI_Init(&argc,&argv);	
-    	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    	MPI_Comm_size(MPI_COMM_WORLD,&numranks);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&numranks);
 
-		if(rank == 0)
+	if(rank == 0)
+	{
+		m = (int*)malloc(SIZE*SIZE*sizeof(int));
+		v = (int*)malloc(1*SIZE*sizeof(int));
+
+		for (int i = 0; i < SIZE*SIZE; i++)
 		{
-			m = (int*)malloc(SIZE*SIZE*sizeof(int));
-			v = (int*)malloc(1*SIZE*sizeof(int));
-
-			for (int i = 0; i < SIZE*SIZE; i++)
+			if (i < SIZE - 1)
 			{
-				if (i < SIZE - 1)
-				{
-					v[i] = i + 1;
-				}
-
-				m [i] = i + 1; 
+				v[i] = i + 1;
 			}
-			
-			result = (int*)malloc(1*SIZE*sizeof(int));
+
+			m [i] = i + 1; 
 		}
+		
+		result = (int*)malloc(1*SIZE*sizeof(int));
+	}
 
-		MPI_Bcast( v, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast( v, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-		//int partition = SIZE/numranks;
-		int* mym = (int*) malloc(1*SIZE*sizeof(int)); //holds the value of the rows 
-		MPI_Scatter(m, SIZE, MPI_INT, mym, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+	int partition = SIZE/numranks;
+	int* mym = (int*) malloc(1*SIZE*sizeof(int)); //holds the value of the rows 
+	MPI_Scatter(m, partition, MPI_INT, mym, partition, MPI_INT, 0, MPI_COMM_WORLD);
 
-		int product = 0;
+	
+	int product = 0;
 
-		for (int i = 0; i < SIZE; i++)
-		{
-			product += mym[i] * v[i];
-		}
+	for (int i = 0; i < SIZE; i++)
+	{
+		product += mym[i] * v[i];
+	}
 
-		MPI_Gather( &product , SIZE , MPI_INT ,  result, SIZE, MPI_INT , 0 , MPI_COMM_WORLD);
+	MPI_Gather( &product , 1 , MPI_INT ,  result, 1, MPI_INT , 0 , MPI_COMM_WORLD);
 
-		for (int i = 0; i < SIZE; i++)
-		{
-			printf("The resul is: %d", result[i]);
-		}
+	for (int i = 0; i < SIZE; i++)
+	{
+		printf("The resul is: %d", result[i]);
+	}
 
-		free(m);
-		free(v);
-		if(rank == 0)
-		{
-			free(result);
-			free(mym);
-		}
+	free(m);
+	free(v);
+
+	if(rank == 0)
+	{
+		free(result);
+		free(mym);
+	}
 
 
-    	MPI_Finalize();
+	MPI_Finalize();
 
-    return 0;
+	return 0;
 }
