11ff33d38273621e5c6cbad934368708cb5efded
AfonsoJ3 <afonsoj3@wit.edu>
2025-02-12 14:06:26 -0500

    PA3 partition logic fixed

--- PA3/PA3.c
+++ PA3/PA3.c
@@ -8,14 +8,23 @@
 int main(int argc, char** argv)
 {
 
-	int rank,numranks ;
+	int rank, numranks, partitionLine, extra;
 
 	int* m; //matrix pointer
 	int* v; //vector pointer
 	int* result;
+
 	MPI_Init(&argc,&argv);	
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&numranks);
+
+	partitionLine = SIZE / numranks;
+	extra = SIZE % numranks;
+	
+	if(rank < extra) 
+	{
+		partitionLine += 1;
+	}
 
 	v = (int*)malloc(1*SIZE*sizeof(int));
 
@@ -40,14 +49,17 @@ int main(int argc, char** argv)
 
 	//int partition = SIZE/numranks;
 	int* mym = (int*) malloc(1*SIZE*sizeof(int)); //holds the value of the rows 
-	MPI_Scatter(m, SIZE, MPI_INT, mym, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Scatter(m, partitionLine, MPI_INT, mym, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
 	
 	int product = 0;
 
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < partitionLine; i++)
 	{
-		product += mym[i] * v[i];
+		for (int j = 0; j < SIZE; i++)
+		{
+			product += mym[i * SIZE + j] * v[j];
+		}
 	}
 
 	MPI_Gather(&product, 1, MPI_INT,  result, 1, MPI_INT, 0, MPI_COMM_WORLD);
