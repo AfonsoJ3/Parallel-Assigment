c0272d0afb34eba8251632ad4851b082bde62023
AfonsoJ3 <afonsoj3@wit.edu>
2025-02-12 22:26:26 -0500

    PA3 restart

--- PA3/PA3.c
+++ PA3/PA3.c
@@ -5,10 +5,49 @@
 //Change it to a bigger mumber. 
 #define SIZE 10
 
+void gen_matrix(int* matrix, int size)
+{
+	for (int i = 0; i < size*size; i++)
+	{
+		matrix [i] = i + 1; 
+	}
+}
+
+void gen_vector(int* vector, int size)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		vector [i] = i + 1; 
+	}
+}
+
+void printMatrix(int* matrix, int size)
+{
+	printf("%d * %d Matrix:\n[ ",size,size);
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size2; j++)
+		{
+			printf("%d ", matrix[i * size + j]); 
+		}
+	}
+	print("]\n");
+}
+
+void printVector(int* vector, int size)
+{
+	printf("%d * 1 Vector:\n[ ",size,size);
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", matrix[i]); 
+		
+	}
+	print("]\n");
+}
+
 int main(int argc, char** argv)
 {
-
-	int rank, numranks, partitionLine, extra;
+	int rank, numranks;
 
 	int* m; //matrix pointer
 	int* v; //vector pointer
@@ -18,51 +57,34 @@ int main(int argc, char** argv)
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 	MPI_Comm_size(MPI_COMM_WORLD,&numranks);
 
-	partitionLine = SIZE / numranks;
-	extra = SIZE % numranks;
-	
-	if(rank < extra) 
-	{
-		partitionLine += 1;
-	}
-
 	v = (int*)malloc(1*SIZE*sizeof(int));
 
 	if(rank == 0)
 	{
 		m = (int*)malloc(SIZE*SIZE*sizeof(int));
-
-		for (int i = 0; i < SIZE*SIZE; i++)
-		{
-			m [i] = i + 1; 
-		}
-
-		for (int i = 0; i < SIZE; i++)
-		{
-			v [i] = i + 1; 
-		}
-		
 		result = (int*)malloc(1*SIZE*sizeof(int));
+		gen_matrix(m, SIZE);
+		gen_vector(v, SIZE)
 	}
 
-	MPI_Bcast(v, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+	// MPI_Bcast(v, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
-	//int partition = SIZE/numranks;
-	int* mym = (int*) malloc(SIZE*SIZE*sizeof(int)); //holds the value of the rows 
-	MPI_Scatter(m, partitionLine * SIZE, MPI_INT, mym,partitionLine * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+	// //int partition = SIZE/numranks;
+	// int* mym = (int*) malloc(SIZE*SIZE*sizeof(int)); //holds the value of the rows 
+	// MPI_Scatter(m, partitionLine * SIZE, MPI_INT, mym,partitionLine * SIZE, MPI_INT, 0, MPI_COMM_WORLD);
 
 	
-	int product = 0;
+	// int product = 0;
 
-	for (int i = 0; i < partitionLine; i++)
-	{
-		for (int j = 0; j < SIZE; j++)
-		{
-			product += mym[i * SIZE + j] * v[j];
-		}
-	}
+	// for (int i = 0; i < partitionLine; i++)
+	// {
+	// 	for (int j = 0; j < SIZE; j++)
+	// 	{
+	// 		product += mym[i * SIZE + j] * v[j];
+	// 	}
+	// }
 
-	MPI_Gather(&product, 1, MPI_INT,  result, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	// MPI_Gather(&product, 1, MPI_INT,  result, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
 	if (rank == 0)
 	{
@@ -72,10 +94,10 @@ int main(int argc, char** argv)
 		}
 
 		free(result);
-		free(mym);
+		free(m);
 	}
 
-	free(m);
+	//free(m);
 	free(v);
 
 	MPI_Finalize();
