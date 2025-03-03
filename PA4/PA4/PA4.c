0717c7edc085d3a0d3ef9724aad6360756613b51
AfonsoJ3 <afonsoj3@wit.edu>
2025-03-03 11:51:39 -0500

    PA4 Test.

---
+++ PA4/PA4/PA4.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "mpi.h"
+
+extern int* imageToMat(char* name, int* dims);
+extern void matToImage(char* namem int* mat, int* dims);
+
+int main(int argc, char* argv[])
+{
+    int* matrix;
+    int* m;
+    int* temp; //temporary file
+    char* name = "image.jpg"; // fale name.
+    int* dims = (int*) malloc(2*sizeof(int)); //image dimension
+    int height;
+    int width;
+
+    int rank, numranks;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+    MPI_Comm_size( MPI_COMM_WORLD , &numranks);
+
+    if (rank == 0)
+    {
+        m = imageToMat(name, dims);
+        height = dims[0];
+        width = dims[1];
+    }
+
+    MPI_Bcast(&height, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank != 0)
+    {
+        int* matrix = (int*) malloc(h*w*sizeof(int)); //memory alocation non root ranks
+    }
+
+    MPI_Bcast(matrix, h * w, MPI_INT, 0, MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
+        printf("The height of image.jpg is: %d\n", height);
+        printf("The width of image.jpg is: %d\n", height);
+
+    }
+
+    if (rank != 0)
+    {
+        free(matrix);
+    }
+
+    free(m);
+    free(dims);
+    free(temp);
+
+
+    MPI_Finalize();
+
+    return 0;
+}
