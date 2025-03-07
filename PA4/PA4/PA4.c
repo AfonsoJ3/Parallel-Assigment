80692e0e9cd9cf9f7e43279589f8ecac682c6d56
AfonsoJ3 <afonsoj3@wit.edu>
2025-03-07 15:12:14 -0500

    PA4 time added

--- PA4/PA4/PA4.c
+++ PA4/PA4/PA4.c
@@ -6,6 +6,8 @@
 extern int* imageToMat(char* name, int* dims);
 extern void matToImage(char* name, int* mat, int* dims);
 
+
+
 int main(int argc, char* argv[])
 {
     int* matrix = NULL;
@@ -15,6 +17,11 @@ int main(int argc, char* argv[])
     int* dims = (int*) malloc(2*sizeof(int)); //image dimension
     int height;
     int width;
+
+    double totalB = 0;
+    double totalB_m = 0;
+    double totalcal = 0;
+    double totalg = 0;
 
     int rank, numranks;
 
@@ -28,16 +35,20 @@ int main(int argc, char* argv[])
         height = dims[0];
         width = dims[1];
     }
+    
 
+    double Bstart = MPI_Wtime();
     MPI_Bcast(&height, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);        
+    double Bend = MPI_Wtime();
 
     if (rank != 0) {
         matrix = (int*) malloc(height * width * sizeof(int));
     }
 
-    
+    double Bstart_m = MPI_Wtime();
     MPI_Bcast(matrix, height * width, MPI_INT, 0, MPI_COMM_WORLD);
+    double Bend_m = MPI_Wtime();
 
     int numRows = height / numranks;
     int myStart = rank * numRows;
@@ -51,6 +62,7 @@ int main(int argc, char* argv[])
     int k = 10;
     temp=(int*) malloc(height*width*sizeof(int));
 
+    double calStart = MPI_Wtime();
     for(int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -76,15 +88,30 @@ int main(int argc, char* argv[])
             temp[index] = sum / counter;
         }
     }
+    double calEnd = MPI_Wtime();
 
+    double gStart = MPI_Wtime();
     MPI_Gather( temp + myStart * width, numRows * width, 
     MPI_INT, temp, numRows * width, MPI_INT, 0, MPI_COMM_WORLD);
+    double gEnd = MPI_Wtime();
+
+    MPI_Reduce(&(Bend - Bstart) , totalB , 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&(Bend_m - Bstart_m) , totalB_m, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&(calEnd - calStart) , totalcal , 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    MPI_Reduce(&(gEnd - gEnd) , totalg , 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
 
     if (rank == 0)
     {
+        totalB = (totalB + totalB_m)/ numranks;
+        totalcal = totalcal / numranks;
+        totalg = totalg / numranks;
+
         matToImage("processedImage.jpg", temp, dims);
-        printf("\nThe height of image.jpg is: %d\n", height);
-        printf("The width of image.jpg is: %d\n", width);
+        printf("The Bcast with %d ranks, took %.10f seconds", numranks, totalB);
+        printf("The calculation with %d ranks, took %.10f seconds", numranks, totalcal);
+        printf("The gather with %d ranks, took %.10f seconds", numranks, totalg);
 
     }
 
