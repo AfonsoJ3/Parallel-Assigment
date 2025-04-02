996bc15d4c7060b3d860b3c6e469bf1ce3a15a47
AfonsoJ3 <afonsoj3@wit.edu>
2025-04-02 11:14:00 -0400

    restart

--- final/mandelbrot.c
+++ final/mandelbrot.c
@@ -1,27 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "mpi.h"
 
 extern void matToImage(char* name, int* mat, int* dims);
 
-int main( int argc, char** argv ) 
-{
+int main( int argc, char** argv ) {
     //setup mandelbrot data
-    int rank, numRank;
-
-    MPI_Init( &argc , &argv);
-    MPI_Comm_size( MPI_COMM_WORLD ,&numRank);
-    MPI_Comm_rank( MPI_COMM_WORLD , &rank);
-    
     int nx=900;
     int ny=600;
-
+    int* matrix=(int*)malloc(nx*ny*sizeof(int));
     double xStart=-2;
     double xEnd=1;
     double yStart=-1;
     double yEnd=1;
     int maxIter=255;
-    int* matrix = NULL;
     //C=x0+iy0
     double x0=0;
     double y0=0;
@@ -31,103 +22,30 @@ int main( int argc, char** argv )
 
     int iter=0;
     
-    int startRow, endRow ;
-    int numele = ny / numRank;
-
-    //Inicially work 
-    if (rank == 0)
-    {
-       matrix = (int*)malloc(nx*ny*sizeof(int));
-       startRow = 0;
-       endRow = numele;
-
-       for (int i = 1; i < numRank - 1 ; i++)
-       {
-            int startingRow =  i * numele;
-            int endingRow = startingRow + numele;
-
-            if (i == numRank - 1)
-            {
-                endingRow= ny;
-            }
-            printf("start Row: %d - end row: %d. ", startingRow, endingRow);
-
-            MPI_Send( &startingRow, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-            MPI_Send( &endingRow, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-       }
-    }
-    else 
-    {
-        MPI_Recv( &startRow, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,  MPI_STATUS_IGNORE);
-        MPI_Recv( &endRow, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,  MPI_STATUS_IGNORE);
-    }
-
-    int* matrix2 = (int*) malloc (nx*(endRow - startRow)*sizeof(int)); 
-
-    #pragma omp parallel for schedule(dynamic) collapse(2)
-    //distrubute the rows evenly among MPI ranks.
-    //use the same method as in PA5.x
     //create mandelbrot here
-    for(int i=startRow;i<endRow;i++)
-    {
-        for(int j=0;j<nx;j++)
-        {
+    for(int i=0;i<ny;i++){
+        for(int j=0;j<nx;j++){
             //chosen a value for C
             x0=xStart+(1.0*j/nx)*(xEnd-xStart);
             y0=yStart+(1.0*i/ny)*(yEnd-yStart);
             x=0; y=0; //set Z to 0
             iter=0;
 
-            while(iter<maxIter)
-            {
+            while(iter<maxIter){
                 iter++;
                 double temp=x*x-y*y+x0;
                 y=2*x*y+y0;
                 x=temp;
                 if(x*x+y*y>4) break;
             }
-            matrix2[(i - startRow)*nx+j]=iter;
+            matrix[i*nx+j]=iter;
         }
     }
 
-   if (rank != 0)
-   {
-     MPI_Send( matrix2, nx*(endRow - startRow), MPI_INT, 0, 0, MPI_COMM_WORLD);
-   }
-    else
-   {
-        for (int i = 1; i < numRank - 1; i++)
-        {
-            int startingRow =  i * numele;
-            int endingRow = startingRow + numele;
+    //save image
+    int dims[2]={ny,nx};
+    matToImage("mandelbrot.jpg",matrix,dims);
 
-            if (i == numRank - 1)
-            {
-                endingRow= ny;
-            }
-            printf("Recive start Row: %d - end row: %d. ", startingRow, endingRow);
-
-            MPI_Recv( matrix + startingRow * nx , nx*(endingRow - startingRow), MPI_INT , i,  0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        }
-
-        for (int i = 0; i < endRow; i++) 
-        {
-            for (int j = 0; j < nx; j++) 
-            {
-                matrix[i * nx + j] = matrix2 [i * nx + j];
-            }
-        }
-
-        for (int i = 0; i < 10; i++) {
-            printf("Row %d: %d\n", i, matrix[i * nx]);
-        }
-
-        int dims[2] = {ny, nx};
-        matToImage("mandelbrot.jpg", matrix, dims);
-        free(matrix);
-   }
-
-    free(matrix2);
-    MPI_Finalize();
+    free(matrix);
     return 0;
 }
