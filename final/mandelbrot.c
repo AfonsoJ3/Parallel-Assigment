09d5ab46fadc526a09dbe485e18af79224a08e8f
AfonsoJ3 <afonsoj3@wit.edu>
2025-04-02 15:50:00 -0400

    fixed the if (i == numRank -1).

--- final/mandelbrot.c
+++ final/mandelbrot.c
@@ -31,18 +31,15 @@ int main( int argc, char** argv )
     int iter=0;
     int* master_Matrix = NULL;
     int numele = nx / numRank;
-    int WmyStart, WmyEnd;
-
     //Sending job to workers
     if (rank == 0)
     {
        master_Matrix = (int*) malloc (nx * ny * sizeof(int));
        
-       for (int i = 0; i < numRank; i++)
+       for (int i = 1; i < numRank; i++)
        {
-            int myStart = i * numele;
-            int myEnd = myStart + numele;
-            
+            int myStart = i * numele + 1;
+            int myEnd = myStart + numele - 1;
             if (i == numRank -1)
             {
                 myEnd = nx;
@@ -55,65 +52,69 @@ int main( int argc, char** argv )
             MPI_Send(&myEnd, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
        }
     }
+    
+    int myStart, myEnd;
+    if (rank != 0)
+    {
+        MPI_Recv( &myStart, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv( &myEnd, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        //printf("Debug: Rank:%d, myStart:%d, myEnd:%d.\n", rank, myStart, myEnd);
+    }
     else
     {
-        MPI_Recv( &WmyStart, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv( &WmyEnd, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+        myStart = 0;
+        myEnd = numele;
         //printf("Debug: Rank:%d, myStart:%d, myEnd:%d.\n", rank, myStart, myEnd);
-        #pragma omp parallel 
+    }
+
+    #pragma omp parallel 
+    {
+        //create mandelbrot here
+        #pragma omp for nowait schedule(dynamic)
+        for(int i = myStart; i < myEnd; i++)
         {
-            //create mandelbrot here
-            #pragma omp for nowait schedule(dynamic)
-            for(int i = WmyStart; i < WmyEnd; i++)
+            //printf("TCS:%d, myStart:%d, myEnd: %d.\n", i, myStart, myEnd);
+
+            for(int j = 0; j < nx; j++)
             {
-                printf("TCS:%d, myStart:%d, myEnd: %d.\n", i, WmyStart, WmyEnd);
-    
-                for(int j = 0; j < nx; j++)
+                //chosen a value for C
+                x0 = xStart + (1.0 * j / nx) * (xEnd - xStart);
+                y0 = yStart + (1.0 * i / ny) * (yEnd - yStart);
+                x = 0; 
+                y = 0;//set Z to 0
+                iter = 0;
+
+                while(iter < maxIter)
                 {
-                    //chosen a value for C
-                    x0 = xStart + (1.0 * j / nx) * (xEnd - xStart);
-                    y0 = yStart + (1.0 * i / ny) * (yEnd - yStart);
-                    x = 0; 
-                    y = 0;//set Z to 0
-                    iter = 0;
-    
-                    while(iter < maxIter)
-                    {
-                        iter++;
-                        double temp = x * x - y * y + x0;
-                        y = 2 * x * y + y0;
-                        x = temp;
-                        if (x * x + y * y > 4) 
-                            break;
-                    }
-                    worker_matrix[(i - WmyStart) * nx + j] = iter;
+                    iter++;
+                    double temp = x * x - y * y + x0;
+                    y = 2 * x * y + y0;
+                    x = temp;
+                    if (x * x + y * y > 4) 
+                        break;
                 }
+                worker_matrix[i * nx + j] = iter;
             }
+
         }
     }
 
-    if (rank == 0)
+    if (rank != 0)
     {
+        MPI_Send(worker_matrix + myStart * nx, (myEnd - myStart) * ny, MPI_INT, 0, 0, MPI_COMM_WORLD);
+
+        printf("worker matrix send.\n");
+    }
+    else
+    {
+        MPI_Recv(&master_Matrix, nx * ny, MPI_INT, 0, MPI_ANY_SOURCE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("matrix recived.\n");
+        
+        //save image
+        int dims[2]={ny,nx};
+        matToImage("mandelbrot.jpg",master_Matrix,dims);
         free(master_Matrix);
     }
-
-    // if (rank != 0)
-    // {
-    //     MPI_Send(worker_matrix, (myEnd - myStart) * ny, MPI_INT, 0, 0, MPI_COMM_WORLD);
-
-    //     printf("worker matrix send.\n");
-    // }
-    // else
-    // {
-    //     MPI_Recv(&master_Matrix, nx * ny, MPI_INT, 0, MPI_ANY_SOURCE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    //     printf("matrix recived.\n");
-        
-    //     //save image
-    //     int dims[2]={ny,nx};
-    //     matToImage("mandelbrot.jpg",master_Matrix,dims);
-    //     free(master_Matrix);
-    // }
 
     free(worker_matrix);
     MPI_Finalize();
